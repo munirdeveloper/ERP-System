cef697e6c5e624a3aa73c751d95ef292556f431e
Munir <munircsdev@gmail.com>
2024-06-23 17:15:09 +0300

    Tailwind ready

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-center text-4xl font-bold text-blue-500">Welcome to SvelteKit</h1>
