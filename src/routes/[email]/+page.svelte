5ac346dd54b1ab30fb102a8d43b72d986fb376b5
Munir <munircsdev@gmail.com>
2024-06-24 13:43:51 +0300

    logic and functionalities for the saving page + api handling

--- src/routes/[email]/+page.svelte
+++ src/routes/[email]/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { getPokemonList, getPokemon } from '$lib/pokemonAPI';
+	export let data;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
 
 	$: email = $page.params.email;
 
@@ -10,6 +14,7 @@
 		description: 'This is your pokemon description.',
 		pokemon_ids: [1, 2, 5]
 	};
+	let isModalOpen = false;
 
 	async function refreshPokemonData() {
 		pokemonData = [];
@@ -25,19 +30,62 @@
 		});
 	}
 
+	async function saveProfile() {
+		const { data: profileData, error: profileError } = await supabase
+			.from('profiles')
+			.select('*')
+			.eq('email', email);
+
+		if (profileData?.length === 0) {
+			// create profile row
+			const { data, error } = await supabase
+				.from('profiles')
+				.insert({ ...profile, user_id: session?.user?.id, email: session?.user?.email });
+		} else {
+			// update profile row
+			const { data, error } = await supabase
+				.from('profiles')
+				.update(profile) // description, pokemon_id
+				.eq('user_id', session?.user?.id);
+		}
+	}
+
 	page.subscribe(async () => {
 		pokemonList = await getPokemonList();
+
+		const { data: profileData, error: profileError } = await supabase
+			.from('profiles')
+			.select('description, pokemon_ids')
+			.eq('email', email);
+
+		if (profileData?.length == 0 && email == session?.user.email) {
+			// save profile
+			console.log('Save profile');
+			await saveProfile();
+		} else if (profileData != null && profileData.length > 0) {
+			profile = profileData[0];
+		} else {
+			console.log('NO Profile');
+			profile = {
+				description: 'This user does not have a profile yet!',
+				pokemon_ids: []
+			};
+		}
+
 		await refreshPokemonData();
-		console.log(await getPokemon('1'));
-		console.log(pokemonList);
 	});
+
+	async function savePageEdits() {
+		console.log('Saving');
+		isModalOpen = false;
+	}
 </script>
 
 <div class="hero min-h-screen bg-blue-400">
 	<div class="hero-content mb-80">
 		<div class="max-w-2xl text-center">
 			<h1 class="text-4xl font-bold text-white">{email}'s Page</h1>
-			<p class="mx-auto max-w-md py-3">User description here..</p>
+			<p class="mx-auto max-w-md py-3">{profile.description}</p>
 			<div class="grid grid-cols-3">
 				{#if pokemonData === undefined}
 					<p>Loading ...</p>
@@ -59,6 +107,34 @@
 					{/each}
 				{/if}
 			</div>
+			{#if email == session?.user?.email}
+				<button class="btn btn-info" on:click={() => (isModalOpen = true)}>Edit Page</button>
+				<dialog class="modal min-w-lg" class:modal-open={isModalOpen}>
+					<div class="modal-box bg-blue-200">
+						<h3>Edit Your PokePage</h3>
+						<p>Here you can make edits to your page, such as description or pokemon selected</p>
+						<p class="p-2 text-white">Edit your description</p>
+						<textarea
+							bind:value={profile.description}
+							class="text-area textarea-bordered textarea-lg h-[300px] w-full max-w-md"
+						/>
+						<button class="btn btn-success" on:click={() => savePageEdits()}>Save Edits</button>
+						<p class="p-2 text-white">Select your pokemon</p>
+						<div class="m-3 grid max-h-[600px] grid-cols-3 overflow-y-scroll">
+							{#each pokemonList as pokemon, index}
+								<button
+									class={'card m-1 h-12 items-center justify-center bg-slate-700 p-1 ' +
+										(profile.pokemon_ids.includes(index + 1) ? 'border-2 border-blue-600' : '')}
+								>
+									<div class="text-center">
+										<h2 class="text-hl text-white">{pokemon.name}</h2>
+									</div>
+								</button>
+							{/each}
+						</div>
+					</div>
+				</dialog>
+			{/if}
 		</div>
 	</div>
 </div>
