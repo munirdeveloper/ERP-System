8fdce9b839ff70b5835d6a66b9306ebcbd67226b
Munir <munircsdev@gmail.com>
2024-06-25 10:55:02 +0300

    vite config fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,27 @@
+// import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-vercel';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	preprocess: vitePreprocess(),
+
+// 	kit: {
+// 		adapter: adapter({
+// 			runtime: 'nodejs20.x'
+// 		})
+// 	}
+// };
+
+// export default config;
+
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
 		adapter: adapter({
 			runtime: 'nodejs20.x'
